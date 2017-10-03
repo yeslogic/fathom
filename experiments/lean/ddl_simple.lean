43b16e7a06aa76ac02ff81d24c082dacf8ff83c1
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2017-10-03 11:54:31 +1100

    Add size calculation

--- experiments/lean/ddl_simple.lean
+++ experiments/lean/ddl_simple.lean
@@ -40,6 +40,7 @@ namespace ddl
 
     /- The type system of the binary language -/
     inductive type : Type
+      | unit : type
       | bit : type
       | sum : type → type → type
       | prod : type → type → type
@@ -47,6 +48,7 @@ namespace ddl
 
     /- embed a binary type into Lean -/
     def type.embed : type → Type
+      | type.unit := unit
       | type.bit := bool
       | (type.sum t₁ t₂) := sum (type.embed t₁) (type.embed t₂)
       | (type.prod t₁ t₂) := type.embed t₁ × type.embed t₂
@@ -54,6 +56,13 @@ namespace ddl
 
     example : type.embed (type.prod type.bit type.bit) = (bool × bool) := rfl
     example : type.embed (type.array type.bit (host.expr.nat 16)) = vector bool 16 := rfl
+
+    def type.max_bits : type → ℕ
+      | type.unit := 0
+      | type.bit := 1
+      | (type.sum t₁ t₂) := max (type.max_bits t₁) (type.max_bits t₂)
+      | (type.prod t₁ t₂) := type.max_bits t₁ + type.max_bits t₂
+      | (type.array t len) := type.max_bits t * host.expr.embed len
 
   end binary
 
