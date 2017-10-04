524ea20ee77edc424828cd13227b5ba8e3d57c37
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2017-10-04 14:35:32 +1100

    Add closed type and some examples

--- experiments/lean/ddl_simple.lean
+++ experiments/lean/ddl_simple.lean
@@ -51,6 +51,13 @@ namespace ddl
       | bool : type
       | nat : type
 
+
+    /- embed a host type into Lean -/
+    def type.embed : type → Type
+      | type.bool := bool
+      | type.nat := ℕ
+
+
     /- The expression syntax of the host language -/
     inductive expr : type → Type
       | bool : bool → expr type.bool
@@ -58,13 +65,10 @@ namespace ddl
       | add : expr type.nat → expr type.nat → expr type.nat
       | mul : expr type.nat → expr type.nat → expr type.nat
 
+
     instance has_coe_to_bool : has_coe bool (expr type.bool) := ⟨expr.bool⟩
     instance has_coe_to_nat : has_coe ℕ (expr type.nat) := ⟨expr.nat⟩
 
-    /- embed a host type into Lean -/
-    def type.embed : type → Type
-      | type.bool := bool
-      | type.nat := ℕ
 
     /- embed a host expression into Lean -/
     def expr.embed : Π {t}, expr t → type.embed t
@@ -72,6 +76,7 @@ namespace ddl
       | _ (expr.nat n)     := n
       | _ (expr.add e₁ e₂) := nat.add (expr.embed e₁) (expr.embed e₂)
       | _ (expr.mul e₁ e₂) := nat.mul (expr.embed e₁) (expr.embed e₂)
+
 
     example : expr.embed (expr.add ↑1 ↑2) = (1 + 2 : ℕ) := rfl
     example : expr.embed (expr.mul ↑4 ↑2) = (4 * 2 : ℕ) := rfl
@@ -83,6 +88,7 @@ namespace ddl
   namespace binary
 
     open range
+
 
     /- Kinds of types in the binary language
 
@@ -96,10 +102,12 @@ namespace ddl
     notation `★` := kind.type
     notation k₁ ` ⇒ ` k₂ := kind.arrow k₁ k₂
 
+
     /- Embed a kind as a Lean term -/
     def kind.embed : kind → Type 1
       | kind.type := Type 0
       | (kind.arrow k₁ k₂) := kind.embed k₁ → kind.embed k₂
+
 
     /- The context in which our types live.
 
@@ -112,6 +120,7 @@ namespace ddl
     def ctx.lookup {n} : fin n → ctx n → kind :=
       flip vector.nth
 
+
     /- The type system of the binary language -/
     inductive type : Π {n}, ctx n → kind → Type
       | var   {n} {Γ : ctx n} (x : fin n) : type Γ (ctx.lookup x Γ)
@@ -123,6 +132,19 @@ namespace ddl
       | abs   {n} {Γ : ctx n} {k₁ k₂}     : type (k₁ :: Γ) k₂ → type Γ (k₁ ⇒ k₂)
       | app   {n} {Γ : ctx n} {k₁ k₂}     : type Γ (k₁ ⇒ k₂) → type Γ k₁ → type Γ k₂ -- FIXME: pop type from Γ?
 
+
+    def type.closed : kind → Type :=
+      type vector.nil
+
+    example : type.closed ★           := type.prod type.bit type.bit
+    example : type.closed ★           := type.array type.bit ↑16
+    example : type.closed (★ ⇒ ★)     := type.abs (type.var 0)
+    example : type.closed ★           := type.app (type.abs (type.var 0)) type.bit
+    example : type.closed (★ ⇒ ★)     := type.app (type.abs (type.var 0)) (type.abs (type.var 0))
+    example : type.closed (★ ⇒ ★ ⇒ ★) := type.abs (type.abs (type.sum (type.var 1) (type.var 0)))
+    example : type.closed (★ ⇒ ★)     := type.app (type.abs (type.abs (type.sum (type.var 1) (type.var 0)))) type.bit
+
+
     /- embed a binary type into Lean -/
     def type.embed : Π {n} (Γ : ctx n) {k : kind}, type Γ k → kind.embed k
       | n Γ k (type.var x)        := sorry
@@ -131,12 +153,14 @@ namespace ddl
       | n Γ k (type.sum t₁ t₂)    := sum (type.embed Γ t₁) (type.embed Γ t₂)
       | n Γ k (type.prod t₁ t₂)   := type.embed Γ t₁ × type.embed Γ t₂
       | n Γ k (type.array t len)  := vector (type.embed Γ t) (host.expr.embed len)
-      | n Γ k (type.abs t)        := λ x, type.embed (_ :: Γ) t
-      | n Γ k (type.app t₁ t₂)    := (type.embed Γ t₁) (type.embed Γ t₂)
+      | n Γ k (type.abs t)        := λ x, type.embed (_ :: Γ) t -- FIXME: Shift
+      | n Γ k (type.app t₁ t₂)    := (type.embed Γ t₁) (type.embed Γ t₂) -- FIXME: Substitute
 
     example : type.embed vector.nil (type.prod type.bit type.bit) = (bool × bool) := rfl
     example : type.embed vector.nil (type.array type.bit ↑16) = vector bool 16 := rfl
 
+
+    /- Get the range of bits that a type might occupy -/
     def type.size : Π {n} (Γ : ctx n) {k : kind}, type Γ k → range
       | n Γ k (type.var x)        := sorry
       | n Γ k type.unit           := ↑0
@@ -147,24 +171,24 @@ namespace ddl
       | n Γ k (type.abs t)        := type.size _ t
       | n Γ k (type.app t₁ t₂)    := type.size _ t₁
 
-    example : type.size vector.nil (type.prod type.bit type.bit) = ↑2 := rfl
-    example : type.size vector.nil (type.prod type.bit type.unit) = ↑1 := rfl
-    example : type.size vector.nil (type.array type.bit ↑16) = ↑16 := rfl
+    example : type.size vector.nil (type.prod type.bit type.bit)  =  ↑2 := rfl
+    example : type.size vector.nil (type.prod type.bit type.unit) =  ↑1 := rfl
+    example : type.size vector.nil (type.array type.bit ↑16)      = ↑16 := rfl
 
-    def read_bits :
-      Π {n : ℕ} (Γ : ctx n)
-        {k : kind} (t : type Γ k)
-        (buf : list bool) {h : list.length buf ∈ type.size Γ t},
-        type.embed Γ t
-      | n Γ k (type.var x)        buf       h := sorry
-      | n Γ k type.unit           buf       h := unit.star
-      | n Γ k type.bit            []        h := sorry
-      | n Γ k type.bit            (x :: xs) h := x
-      | n Γ k (type.sum t₁ t₂)    buf       h := sorry
-      | n Γ k (type.prod t₁ t₂)   buf       h := sorry
-      | n Γ k (type.array t len)  buf       h := sorry
-      | n Γ k (type.abs _)        buf       h := sorry
-      | n Γ k (type.app _ _)      buf       h := sorry
+
+    /- Read a stream of bits into a Lean term using the type to control
+       deserialization
+    -/
+    def read_bits : Π {n} (Γ : ctx n) {k} (t : type Γ k), list bool → type.embed Γ t
+      | n Γ k (type.var x)        buf       := sorry
+      | n Γ k type.unit           buf       := unit.star
+      | n Γ k type.bit            []        := sorry
+      | n Γ k type.bit            (x :: xs) := x
+      | n Γ k (type.sum t₁ t₂)    buf       := sorry
+      | n Γ k (type.prod t₁ t₂)   buf       := sorry
+      | n Γ k (type.array t len)  buf       := sorry
+      | n Γ k (type.abs _)        buf       := sorry
+      | n Γ k (type.app _ _)      buf       := sorry
 
   end binary
 
