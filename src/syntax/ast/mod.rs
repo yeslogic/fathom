61e22b414c4f39b9c9af3d01b274be389ea07484
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-01-19 09:31:16 +1100

    Reorganise the AST module so that it reads top-down

--- src/syntax/ast/mod.rs
+++ src/syntax/ast/mod.rs
@@ -10,88 +10,6 @@ use var::ScopeIndex;
 pub mod binary;
 pub mod host;
 
-/// A field in a struct type
-#[derive(Debug, Clone)]
-pub struct Field<T> {
-    /// Doc comment
-    ///
-    /// Note: This is ignored for comparison purposes
-    pub doc: Rc<str>,
-    /// The name of the field
-    pub name: String,
-    /// The value that this field is associated with
-    pub value: T,
-}
-
-impl<T> Field<T> {
-    /// Apply the function `f` to the field value and return the wrapped result
-    pub fn map_value<U, F: FnMut(T) -> U>(self, mut f: F) -> Field<U> {
-        Field {
-            doc: self.doc,
-            name: self.name,
-            value: f(self.value),
-        }
-    }
-}
-
-impl<T: PartialEq> PartialEq for Field<T> {
-    fn eq(&self, other: &Field<T>) -> bool {
-        // Ignoring doc commment
-        self.name == other.name && self.value == other.value
-    }
-}
-
-fn lookup_field<'a, T>(fields: &'a [Field<T>], name: &str) -> Option<&'a T> {
-    fields
-        .iter()
-        .find(|field| &field.name == name)
-        .map(|field| &field.value)
-}
-
-/// A type definition
-///
-/// ```plain
-/// Point = struct {
-///     x : u16,
-///     y : u16,
-/// }
-/// ```
-#[derive(Debug, Clone)]
-pub struct Definition {
-    /// Doc comment
-    ///
-    /// Note: This is ignored for comparison purposes
-    pub doc: Rc<str>,
-    /// The name of the defined type
-    pub name: String,
-    /// The binary type
-    pub body_ty: binary::RcType,
-}
-
-impl Definition {
-    pub fn from_parse(src: &ParseDefinition) -> Result<Definition, ()> {
-        let body_ty = binary::RcType::from_parse(&src.body_ty)?;
-
-        Ok(Definition {
-            doc: src.doc.join("\n").into(),
-            name: String::from(src.name),
-            body_ty: match &src.param_names[..] {
-                names if names.is_empty() => body_ty,
-                names => binary::RcType::lam(src.span, names, body_ty),
-            },
-        })
-    }
-}
-
-impl PartialEq for Definition {
-    fn eq(&self, other: &Definition) -> bool {
-        // Ignoring doc commment
-        self.name == other.name && self.body_ty == other.body_ty
-    }
-}
-
-pub type Substitutions = BTreeMap<String, binary::RcType>;
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Module {
     pub definitions: Vec<Definition>,
@@ -164,4 +82,86 @@ pub fn base_defs() -> Substitutions {
         "f32be".into() => Type::Const(TypeConst::F32(Endianness::Big)).into(),
         "f64be".into() => Type::Const(TypeConst::F64(Endianness::Big)).into(),
     }
+}
+
+pub type Substitutions = BTreeMap<String, binary::RcType>;
+
+/// A type definition
+///
+/// ```plain
+/// Point = struct {
+///     x : u16,
+///     y : u16,
+/// }
+/// ```
+#[derive(Debug, Clone)]
+pub struct Definition {
+    /// Doc comment
+    ///
+    /// Note: This is ignored for comparison purposes
+    pub doc: Rc<str>,
+    /// The name of the defined type
+    pub name: String,
+    /// The binary type
+    pub body_ty: binary::RcType,
+}
+
+impl Definition {
+    pub fn from_parse(src: &ParseDefinition) -> Result<Definition, ()> {
+        let body_ty = binary::RcType::from_parse(&src.body_ty)?;
+
+        Ok(Definition {
+            doc: src.doc.join("\n").into(),
+            name: String::from(src.name),
+            body_ty: match &src.param_names[..] {
+                names if names.is_empty() => body_ty,
+                names => binary::RcType::lam(src.span, names, body_ty),
+            },
+        })
+    }
+}
+
+impl PartialEq for Definition {
+    fn eq(&self, other: &Definition) -> bool {
+        // Ignoring doc commment
+        self.name == other.name && self.body_ty == other.body_ty
+    }
+}
+
+/// A field in a struct type
+#[derive(Debug, Clone)]
+pub struct Field<T> {
+    /// Doc comment
+    ///
+    /// Note: This is ignored for comparison purposes
+    pub doc: Rc<str>,
+    /// The name of the field
+    pub name: String,
+    /// The value that this field is associated with
+    pub value: T,
+}
+
+impl<T> Field<T> {
+    /// Apply the function `f` to the field value and return the wrapped result
+    pub fn map_value<U, F: FnMut(T) -> U>(self, mut f: F) -> Field<U> {
+        Field {
+            doc: self.doc,
+            name: self.name,
+            value: f(self.value),
+        }
+    }
+}
+
+impl<T: PartialEq> PartialEq for Field<T> {
+    fn eq(&self, other: &Field<T>) -> bool {
+        // Ignoring doc commment
+        self.name == other.name && self.value == other.value
+    }
+}
+
+fn lookup_field<'a, T>(fields: &'a [Field<T>], name: &str) -> Option<&'a T> {
+    fields
+        .iter()
+        .find(|field| &field.name == name)
+        .map(|field| &field.value)
 }
