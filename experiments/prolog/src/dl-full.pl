1d0f1350637e8cd2505b10994e4601c74da67566
Mark Brown <mark@mercurylang.org>
2018-02-12 03:48:38 +1100

    Implement DL-ARRAY.

--- experiments/prolog/src/dl-full.pl
+++ experiments/prolog/src/dl-full.pl
@@ -17,6 +17,9 @@ repr(M, G, sigma(X, D1, D2), pair(X, T1, T2)) :-
     name(X),
     repr(M, G, D1, T1),
     repr(M, [X - T1 | G], D2, T2).
+repr(M, G, array(D, E), array(T)) :-
+    has_type(G, E, int),
+    repr(M, G, D, T).
 
 
 %%%%%%%%%%%
@@ -35,6 +38,15 @@ has_type(G, epair(X, E1, E2), pair(X, T1, T2)) :-
 has_type(G, efield(E, X), TX) :-
     has_type(G, E, T),
     field_type(T, X, TX).
+has_type(_, enil, array(_)).
+has_type(G, econs(E1, E2), array(T)) :-
+    has_type(G, E1, T),
+    has_type(G, E2, array(T)).
+has_type(G, elength(E), int) :-
+    has_type(G, E, array(_)).
+has_type(G, eindex(E1, E2), T) :-
+    has_type(G, E1, array(T)),
+    has_type(G, E2, int).
 
 
 %%%%%%%%%%%%
@@ -54,6 +66,19 @@ parse(_, _, unit, enil).
 parse(Sub, In, sigma(X, D1, D2), epair(X, E1, E2)) :-
     parse(Sub, In, D1, E1),
     parse([X - E1 | Sub], In, D2, E2).
+parse(Sub, In, array(D, EN), E) :-
+    eval(Sub, EN, eint(N)),
+    parse_array(Sub, In, D, N, E).
+
+parse_array(Sub, In, D, N, E) :-
+    ( N > 0 ->
+        parse(Sub, In, D, E0),
+        N1 is N - 1,
+        parse_array(Sub, In, D, N1, E1),
+        E = econs(E0, E1)
+    ;
+        E = enil
+    ).
 
 
 %%%%%%%%%%%%%%%%
@@ -70,3 +95,14 @@ eval(Sub, epair(E1, E2), epair(V1, V2)) :-
 eval(Sub, efield(E, X), VX) :-
     eval(Sub, E, V),
     field_expr(V, X, VX).
+eval(Sub, econs(E1, E2), econs(V1, V2)) :-
+    eval(Sub, E1, V1),
+    eval(Sub, E2, V2).
+eval(Sub, elength(E), eint(N)) :-
+    eval(Sub, E, V),
+    array_length(V, N).
+eval(Sub, eindex(E1, E2), V) :-
+    eval(Sub, E1, V1),
+    eval(Sub, E2, eint(N)),
+    array_index(V1, N, V).
+
--- experiments/prolog/src/examples.pl
+++ experiments/prolog/src/examples.pl
@@ -1,3 +1,7 @@
+run :-
+    runtest(_),
+    fail.
+
 runtest(Name) :-
     ex(Name, D, Ext),
     format('Test: ~w~n', [Name]),
@@ -51,6 +55,18 @@ write_expr(epair(X, E1, E2)) :-
 write_expr(efield(E, X)) :-
     write_expr(E),
     format('.~w', [X]).
+write_expr(econs(E1, E2)) :-
+    write('['),
+    write_expr(E1),
+    write_array(E2).
+write_expr(elength(E)) :-
+    write_expr(E),
+    write('.length').
+write_expr(eindex(E1, E2)) :-
+    write_expr(E1),
+    write('['),
+    write_expr(E2),
+    write(']').
 
 write_struct(enil) :-
     write(' }').
@@ -58,6 +74,13 @@ write_struct(epair(X, E1, E2)) :-
     format(', ~w: ', [X]),
     write_expr(E1),
     write_struct(E2).
+
+write_array(enil) :-
+    write(']').
+write_array(econs(E1, E2)) :-
+    write(', '),
+    write_expr(E1),
+    write_array(E2).
 
 
 %%%%%%%%%%%%%%
@@ -67,6 +90,8 @@ write_struct(epair(X, E1, E2)) :-
 inttype(B, int(B, eint(L), eint(U))) :-
     L is -(2**(B-1)),
     U is (2**(B-1))-1.
+
+% DL-1
 
 ex(xyz, D, 'bin') :-
     inttype(16, Int16),
@@ -83,3 +108,33 @@ ex(xy_z, D, 'bin') :-
             unit)),
         sigma(z, Int16,
         unit)).
+
+% DL-ARRAY
+
+ex(sudoku1, D, 'bin') :-
+    inttype(8, Int8),
+    inttype(16, Int16),
+    inttype(32, Int32),
+    D = sigma(initcount, Int32,
+        sigma(initvalues,
+            array(
+                sigma(x, Int8,
+                sigma(y, Int8,
+                sigma(n, Int16,
+                unit))),
+                evar(initcount)
+            ),
+        unit)).
+
+ex(sudoku2, D, 'bin') :-
+    D = sigma(initcount, int(32, eint(1), eint(80)),
+        sigma(initvalues,
+            array(
+                sigma(x, int(8, eint(0), eint(8)),
+                sigma(y, int(8, eint(0), eint(8)),
+                sigma(n, int(16, eint(1), eint(9)),
+                unit))),
+                evar(initcount)
+            ),
+        unit)).
+
