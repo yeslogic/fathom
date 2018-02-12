8f6aa76d491c28b5a742cd61fe4e4f7ff66b1dc4
Mark Brown <mark@mercurylang.org>
2018-02-12 13:56:57 +1100

    Document DL-* examples; add example for DL-ARITH.

--- experiments/prolog/src/examples.pl
+++ experiments/prolog/src/examples.pl
@@ -8,6 +8,7 @@ inttype(B, int(B, eint(L), eint(U))) :-
 
 % DL-1
 
+% struct { x: int16; y: int16; z: int16; }
 ex(xyz, D, 'bin') :-
     inttype(16, Int16),
     D = sigma(x, Int16,
@@ -15,6 +16,7 @@ ex(xyz, D, 'bin') :-
         sigma(z, Int16,
         unit))).
 
+% struct { xy: struct { x: int16; y: int16; }; z: int16; }
 ex(xy_z, D, 'bin') :-
     inttype(16, Int16),
     D = sigma(xy,
@@ -26,6 +28,10 @@ ex(xy_z, D, 'bin') :-
 
 % DL-ARRAY
 
+% struct {
+%   initcount: int32;
+%   initvalues: struct { x: int8; y: int8; n: int16; }[initcount];
+% }
 ex(sudoku1, D, 'bin') :-
     inttype(8, Int8),
     inttype(16, Int16),
@@ -41,6 +47,15 @@ ex(sudoku1, D, 'bin') :-
             ),
         unit)).
 
+% struct {
+%   initcount: int32 in 1..80;
+%   initvalues:
+%       struct {
+%           x: int8 in 0..8;
+%           y: int8 in 0..8;
+%           n: int16 in 1..9;
+%       }[initcount];
+% }
 ex(sudoku2, D, 'bin') :-
     D = sigma(initcount, int(32, eint(1), eint(80)),
         sigma(initvalues,
@@ -53,7 +68,45 @@ ex(sudoku2, D, 'bin') :-
             ),
         unit)).
 
+% DL-ARITH+ARRAY
+
+% struct {
+%   size: int16 in 1..0x7f;
+%   initcount: int16 in 1..(size*size - 1);
+%   initvalues:
+%       struct {
+%           x: int8 in 0..(size - 1);
+%           y: int8 in 0..(size - 1);
+%           n: int16 in 1..size;
+%       }[initcount];
+% }
 ex(sudoku3, D, 'bin') :-
+    MaxCount = eplus(etimes(evar(size), evar(size)), eint(-1)),
+    MaxPos = eplus(evar(size), eint(-1)),
+    D = sigma(size, int(16, eint(1), eint(0x7f)),
+        sigma(initcount, int(16, eint(1), MaxCount),
+        sigma(initvalues,
+            array(
+                sigma(x, int(8, eint(0), MaxPos),
+                sigma(y, int(8, eint(0), MaxPos),
+                sigma(n, int(16, eint(1), evar(size)),
+                unit))),
+                evar(initcount)
+            ),
+        unit))).
+
+% DL-UNSIGNED+ARRAY
+
+% struct {
+%   initcount: uint32 in 1..80;
+%   initvalues:
+%       struct {
+%           x: uint8 in 0..8;
+%           y: uint8 in 0..8;
+%           n: uint16 in 1..9;
+%       }[initcount];
+% }
+ex(sudoku4, D, 'bin') :-
     D = sigma(initcount, uint(32, eint(1), eint(80)),
         sigma(initvalues,
             array(
