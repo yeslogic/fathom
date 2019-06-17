7d771a179b9c7e8d7dc539a57b7734cc1e51311e
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-06-17 10:36:57 +1000

    Move snapshot stuff to submodule

--- crates/ddl-test/src/support/mod.rs
+++ crates/ddl-test/src/support/mod.rs
@@ -1,11 +1,11 @@
 use codespan::{FileId, Files};
 use codespan_reporting::termcolor::{BufferWriter, ColorChoice, StandardStream};
 use codespan_reporting::{self, Diagnostic, Severity};
-use difference::Changeset;
+use std::fs;
 use std::path::{Path, PathBuf};
-use std::{fmt, fs, io, str};
 
 mod directives;
+mod snapshot;
 
 use self::directives::Directives;
 
@@ -84,7 +84,7 @@ pub fn run_test(test_name: &str, test_path: &str) {
             let mut output = Vec::new();
             let mut diagnostics = ddl_compile_rust::compile_module(&mut output, &module).unwrap();
 
-            if let Err(error) = compare_snapshot(&test_path, "rs", &output) {
+            if let Err(error) = snapshot::compare(&test_path, "rs", &output) {
                 failed_checks.push("compile_rust: snapshot");
 
                 eprintln!("Failed COMPILE/RUST snapshot test:");
@@ -116,7 +116,7 @@ pub fn run_test(test_name: &str, test_path: &str) {
             let mut output = Vec::new();
             let mut diagnostics = ddl_compile_doc::compile_module(&mut output, &module).unwrap();
 
-            if let Err(error) = compare_snapshot(&test_path, "md", &output) {
+            if let Err(error) = snapshot::compare(&test_path, "md", &output) {
                 failed_checks.push("compile_doc: snapshot");
 
                 eprintln!("Failed COMPILE/DOC snapshot test:");
@@ -183,96 +183,6 @@ pub fn run_test(test_name: &str, test_path: &str) {
         eprintln!();
 
         panic!("failed {}", test_name);
-    }
-}
-
-fn compare_snapshot(path: &Path, extension: &str, found_bytes: &[u8]) -> Result<(), SnapshotError> {
-    use std::env;
-
-    let out_path = path.with_extension(extension);
-    let found_str = std::str::from_utf8(found_bytes).map_err(SnapshotError::OutputUtf8)?;
-
-    let is_bless = env::var("DDL_BLESS").is_ok();
-
-    if out_path.exists() {
-        let expected_string = read_snapshot(&out_path)?;
-        let changeset = Changeset::new(&expected_string, found_str, "\n");
-
-        if !changeset.diffs.is_empty() {
-            if is_bless {
-                bless_snapshot(out_path, found_str)?;
-            } else {
-                return Err(SnapshotError::UnexpectedChangesFound(out_path, changeset));
-            }
-        }
-    } else {
-        if is_bless {
-            bless_snapshot(out_path, found_str)?;
-        } else {
-            return Err(SnapshotError::ExistingSnapshotNotFound(out_path));
-        }
-    }
-
-    Ok(())
-}
-
-fn read_snapshot(out_path: &Path) -> Result<String, SnapshotError> {
-    fs::read_to_string(&out_path)
-        .map_err(|error| SnapshotError::ReadSnapshot(out_path.to_owned(), error))
-}
-
-fn bless_snapshot(out_path: PathBuf, found_str: &str) -> Result<(), SnapshotError> {
-    fs::write(&out_path, found_str).map_err(|error| SnapshotError::WriteSnapshot(out_path, error))
-}
-
-enum SnapshotError {
-    OutputUtf8(str::Utf8Error),
-    ReadSnapshot(PathBuf, io::Error),
-    WriteSnapshot(PathBuf, io::Error),
-    ExistingSnapshotNotFound(PathBuf),
-    UnexpectedChangesFound(PathBuf, Changeset),
-}
-
-impl fmt::Display for SnapshotError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            SnapshotError::OutputUtf8(error) => writeln!(f, "actual output not utf8: {}", error)?,
-            SnapshotError::ReadSnapshot(path, error) => {
-                writeln!(f, "error reading snapshot `{}`: {}", path.display(), error)?;
-            }
-            SnapshotError::WriteSnapshot(path, error) => {
-                writeln!(f, "error writing snapshot `{}`: {}", path.display(), error)?;
-            }
-            SnapshotError::ExistingSnapshotNotFound(path) => {
-                writeln!(f, "existing snapshot `{}` not found", path.display())?;
-                writeln!(f)?;
-                writeln!(
-                    f,
-                    "note: Run with `DDL_BLESS=1` environment variable to regenerate."
-                )?;
-                writeln!(f)?;
-            }
-            SnapshotError::UnexpectedChangesFound(path, changeset) => {
-                use difference::Difference as Diff;
-                writeln!(f, "changes found in snapshot `{}`: ", path.display())?;
-                writeln!(f)?;
-                for diff in &changeset.diffs {
-                    match diff {
-                        // TODO: Colored diffs
-                        Diff::Same(data) => writeln!(f, "      {}", data.replace('\n', "¶"))?,
-                        Diff::Add(data) => writeln!(f, "    + {}", data.replace('\n', "¶"))?,
-                        Diff::Rem(data) => writeln!(f, "    - {}", data.replace('\n', "¶"))?,
-                    }
-                }
-                writeln!(f)?;
-                writeln!(
-                    f,
-                    "note: Run with `DDL_BLESS=1` environment variable to regenerate."
-                )?;
-                writeln!(f)?;
-            }
-        }
-        Ok(())
     }
 }
 
