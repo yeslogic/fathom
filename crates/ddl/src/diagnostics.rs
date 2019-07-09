e15131fb0a88d0876449bf6aecb9f0f9d15c9935
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-07-09 16:48:22 +1000

    Move parse error diagnostic construction

--- crates/ddl/src/diagnostics.rs
+++ crates/ddl/src/diagnostics.rs
@@ -1,7 +1,11 @@
 //! Diagnostics.
 
-use codespan::{FileId, Span};
+use codespan::{FileId, Span, ByteIndex};
 use codespan_reporting::diagnostic::{Diagnostic, Label, Severity};
+use lalrpop_util::ParseError;
+use std::fmt;
+
+use crate::lexer::Token;
 
 pub fn field_redeclaration(
     severity: Severity,
@@ -59,4 +63,63 @@ pub fn var_name_not_found(
         // TODO: provide suggestions
         notes: vec![],
     }
+}
+
+pub fn parse_error(
+    file_id: FileId,
+    error: ParseError<ByteIndex, Token, Diagnostic>,
+) -> Diagnostic {
+    match error {
+        ParseError::InvalidToken { location: _ } => unreachable!(),
+
+        ParseError::UnrecognizedEOF { location, expected } => Diagnostic::new_error(
+            "unexpected end of file",
+            Label::new(file_id, location..location, "unexpected end of file"),
+        )
+        .with_notes(vec![format!(
+            "expected one of {}",
+            display_expected(&expected),
+        )]),
+
+        ParseError::UnrecognizedToken {
+            token: (start, token, end),
+            expected,
+        } => Diagnostic::new_error(
+            format!("unexpected token \"{}\"", token),
+            Label::new(file_id, start..end, "unexpected token"),
+        )
+        .with_notes(vec![format!(
+            "expected one of {}",
+            display_expected(&expected),
+        )]),
+
+        ParseError::ExtraToken {
+            token: (start, token, end),
+        } => Diagnostic::new_error(
+            format!("extra token \"{}\"", token),
+            Label::new(file_id, start..end, "extra token"),
+        ),
+
+        ParseError::User { error } => error,
+    }
+}
+
+fn display_expected<'a, Item: fmt::Display>(items: &'a [Item]) -> impl 'a + fmt::Display {
+    struct DisplayExpected<'a, Item>(&'a [Item]);
+
+    impl<'a, Item: fmt::Display> fmt::Display for DisplayExpected<'a, Item> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            for (i, item) in self.0.iter().enumerate() {
+                match i {
+                    0 => write!(f, "{}", item)?,
+                    i if i >= self.0.len() => write!(f, ", or {}", item)?,
+                    _ => write!(f, ", {}", item)?,
+                }
+            }
+
+            Ok(())
+        }
+    }
+
+    DisplayExpected(items)
 }
--- crates/ddl/src/parse/mod.rs
+++ crates/ddl/src/parse/mod.rs
@@ -1,12 +1,11 @@
 //! Parser for the data description language.
 
-use codespan::{ByteIndex, FileId};
-use codespan_reporting::diagnostic::{Diagnostic, Label};
-use lalrpop_util::ParseError;
-use std::fmt;
+use codespan::{FileId};
+use codespan_reporting::diagnostic::{Diagnostic};
 
 use crate::concrete::Module;
-use crate::lexer::{Lexer, Token};
+use crate::lexer::Lexer;
+use crate::diagnostics;
 
 mod grammar {
     include!(concat!(env!("OUT_DIR"), "/parse/grammar.rs"));
@@ -20,7 +19,7 @@ pub fn parse_module(
     let module = grammar::ModuleParser::new()
         .parse(file_id, report, lexer)
         .unwrap_or_else(|error| {
-            report(parse_error_to_diagnostic(file_id, error));
+            report(diagnostics::parse_error(file_id, error));
             Module {
                 file_id,
                 items: Vec::new(),
@@ -28,64 +27,4 @@ pub fn parse_module(
         });
 
     module
-}
-
-/// Convert an LALRPOP error to a codespan-reporting diagnostic.
-fn parse_error_to_diagnostic(
-    file_id: FileId,
-    error: ParseError<ByteIndex, Token, Diagnostic>,
-) -> Diagnostic {
-    match error {
-        ParseError::InvalidToken { location: _ } => unreachable!(),
-
-        ParseError::UnrecognizedEOF { location, expected } => Diagnostic::new_error(
-            "unexpected end of file",
-            Label::new(file_id, location..location, "unexpected end of file"),
-        )
-        .with_notes(vec![format!(
-            "expected one of {}",
-            display_expected(&expected),
-        )]),
-
-        ParseError::UnrecognizedToken {
-            token: (start, token, end),
-            expected,
-        } => Diagnostic::new_error(
-            format!("unexpected token \"{}\"", token),
-            Label::new(file_id, start..end, "unexpected token"),
-        )
-        .with_notes(vec![format!(
-            "expected one of {}",
-            display_expected(&expected),
-        )]),
-
-        ParseError::ExtraToken {
-            token: (start, token, end),
-        } => Diagnostic::new_error(
-            format!("extra token \"{}\"", token),
-            Label::new(file_id, start..end, "extra token"),
-        ),
-
-        ParseError::User { error } => error,
-    }
-}
-
-fn display_expected<'a, Item: fmt::Display>(items: &'a [Item]) -> impl 'a + fmt::Display {
-    struct DisplayExpected<'a, Item>(&'a [Item]);
-
-    impl<'a, Item: fmt::Display> fmt::Display for DisplayExpected<'a, Item> {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            for (i, item) in self.0.iter().enumerate() {
-                match i {
-                    0 => write!(f, "{}", item)?,
-                    i if i >= self.0.len() => write!(f, ", or {}", item)?,
-                    _ => write!(f, ", {}", item)?,
-                }
-            }
-
-            Ok(())
-        }
-    }
-
-    DisplayExpected(items)
 }
