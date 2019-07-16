584367b1a6fc9a5197855d3ee60c15bac06cc167
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-07-16 12:40:45 +1000

    Add elaboration and validation contexts
    This mirrors the specification more closely. We’ll eventually have more information in these contexts, for instance for keeping track of type annotations, definitions, and globals.

--- crates/ddl/src/elaborate/mod.rs
+++ crates/ddl/src/elaborate/mod.rs
@@ -19,37 +19,71 @@ pub fn elaborate_module(
     concrete_module: &concrete::Module,
     report: &mut dyn FnMut(Diagnostic),
 ) -> core::Module {
-    let file_id = concrete_module.file_id;
+    let item_context = ItemContext::new(concrete_module.file_id);
+    core::Module {
+        file_id: concrete_module.file_id,
+        items: elaborate_items(item_context, &concrete_module.items, report),
+    }
+}
 
-    let mut used_names = HashMap::new();
-    let mut core_module = core::Module {
-        file_id,
-        items: Vec::new(),
-    };
+/// Contextual information to be used when elaborating items.
+pub struct ItemContext {
+    /// The file where these items are defined (for error reporting).
+    file_id: FileId,
+    /// Labels that have previously been used for items, along with the span
+    /// where they were introduced (for error reporting).
+    labels: HashMap<core::Label, Span>,
+}
 
-    for item in concrete_module.items.iter() {
+impl ItemContext {
+    /// Create a new item context.
+    pub fn new(file_id: FileId) -> ItemContext {
+        ItemContext {
+            file_id,
+            labels: HashMap::new(),
+        }
+    }
+
+    /// Create a field context based on this item context.
+    pub fn field_context(&self) -> FieldContext {
+        FieldContext::new(self.file_id)
+    }
+}
+
+/// Elaborate items in the concrete syntax into items in the core syntax.
+pub fn elaborate_items(
+    mut context: ItemContext,
+    concrete_items: &[concrete::Item],
+    report: &mut dyn FnMut(Diagnostic),
+) -> Vec<core::Item> {
+    let mut core_items = Vec::new();
+
+    for item in concrete_items.iter() {
         use std::collections::hash_map::Entry;
 
         match item {
             concrete::Item::Struct(struct_ty) => {
-                let core_fields = elaborate_struct_ty_fields(file_id, &struct_ty.fields, report);
+                let label = core::Label(struct_ty.name.to_string());
+                let field_context = context.field_context();
+                let core_fields =
+                    elaborate_struct_ty_fields(field_context, &struct_ty.fields, report);
 
-                match used_names.entry(struct_ty.name.as_str()) {
+                match context.labels.entry(label) {
                     Entry::Vacant(entry) => {
                         let item = core::StructType {
                             span: struct_ty.span,
                             doc: struct_ty.doc.clone(),
-                            name: core::Label(struct_ty.name.to_string()),
+                            name: entry.key().clone(),
                             fields: core_fields,
                         };
 
-                        core_module.items.push(core::Item::Struct(item));
+                        core_items.push(core::Item::Struct(item));
                         entry.insert(struct_ty.span);
                     }
                     Entry::Occupied(entry) => report(diagnostics::item_redefinition(
                         Severity::Error,
-                        file_id,
-                        struct_ty.name.as_str(),
+                        context.file_id,
+                        entry.key(),
                         struct_ty.span,
                         *entry.get(),
                     )),
@@ -58,49 +92,89 @@ pub fn elaborate_module(
         }
     }
 
-    core_module
+    core_items
 }
 
-pub fn elaborate_struct_ty_fields(
+/// Contextual information to be used when elaborating structure type fields.
+pub struct FieldContext {
+    /// The file where these fields are defined (for error reporting).
     file_id: FileId,
+    /// Labels that have previously been used for fields, along with the span
+    /// where they were introduced (for error reporting).
+    labels: HashMap<core::Label, Span>,
+}
+
+impl FieldContext {
+    /// Create a new field context.
+    pub fn new(file_id: FileId) -> FieldContext {
+        FieldContext {
+            file_id,
+            labels: HashMap::new(),
+        }
+    }
+
+    /// Create a term context based on this field context.
+    pub fn term_context(&self) -> TermContext {
+        TermContext::new(self.file_id)
+    }
+}
+
+/// Elaborate structure type fields in the concrete syntax into structure type
+/// fields in the core syntax.
+pub fn elaborate_struct_ty_fields(
+    mut context: FieldContext,
     concrete_fields: &[concrete::TypeField],
     report: &mut dyn FnMut(Diagnostic),
 ) -> Vec<core::TypeField> {
-    let mut used_field_names = HashMap::new();
     let mut core_fields = Vec::with_capacity(concrete_fields.len());
 
     for field in concrete_fields {
         use std::collections::hash_map::Entry;
 
+        let label = core::Label(field.name.to_string());
         let field_span = Span::merge(field.name.span(), field.term.span());
-        match used_field_names.entry(field.name.as_str()) {
+        let ty = elaborate_ty(context.term_context(), &field.term, report);
+
+        match context.labels.entry(label) {
             Entry::Vacant(entry) => {
-                entry.insert(field_span);
                 core_fields.push(core::TypeField {
                     doc: field.doc.clone(),
                     start: field_span.start(),
-                    name: core::Label(field.name.to_string()),
-                    term: check_term_ty(file_id, &field.term, report),
+                    name: entry.key().clone(),
+                    term: ty,
                 });
+
+                entry.insert(field_span);
             }
-            Entry::Occupied(entry) => {
-                report(diagnostics::field_redeclaration(
-                    Severity::Error,
-                    file_id,
-                    field.name.as_str(),
-                    field_span,
-                    *entry.get(),
-                ));
-            }
+            Entry::Occupied(entry) => report(diagnostics::field_redeclaration(
+                Severity::Error,
+                context.file_id,
+                entry.key(),
+                field_span,
+                *entry.get(),
+            )),
         }
     }
 
     core_fields
 }
 
-/// Check that a concrete term is a type, and elaborate it into the core syntax.
-pub fn check_term_ty(
+/// Contextual information to be used when elaborating terms.
+pub struct TermContext {
+    /// The file where this term is located (for error reporting).
     file_id: FileId,
+}
+
+impl TermContext {
+    /// Create a new term context.
+    pub fn new(file_id: FileId) -> TermContext {
+        TermContext { file_id }
+    }
+}
+
+/// Check that a concrete term is a type, and elaborate it into the core syntax.
+pub fn elaborate_ty(
+    context: TermContext,
     concrete_term: &concrete::Term,
     report: &mut dyn FnMut(Diagnostic),
 ) -> core::Term {
@@ -123,7 +197,7 @@ pub fn check_term_ty(
             _ => {
                 report(diagnostics::var_name_not_found(
                     Severity::Error,
-                    file_id,
+                    context.file_id,
                     name.as_str(),
                     name.span(),
                 ));
