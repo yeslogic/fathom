1e298b9d721c1c0ecae6596c716480bd5e9d083a
Paul <423357+toothbrush@users.noreply.github.com>
2020-10-07 15:31:31 +0930

    Add the output specifiers.

--- fathom-cli/src/cli.rs
+++ fathom-cli/src/cli.rs
@@ -18,6 +18,27 @@ pub struct Cli {
     /// will be used to process the files.
     #[structopt(long, group = "format_choice")]
     format_file: Option<PathBuf>,
+    // Output options, optional:
+    /// Prints a brief summary of each file based on the format
+    /// description. This may include printing the type or version or
+    /// other important details about the file but may not validate
+    /// the entire file contents.
+    #[structopt(long)]
+    summary: bool,
+    /// Prints a textual representation of the complete contents of
+    /// each file as described by the format description. This will
+    /// also validate the entire file against the format description
+    /// and warn of any discrepancies.
+    #[structopt(long)]
+    dump: bool,
+    /// Print the results of applying the specified format query to
+    /// each input file.
+    #[structopt(long)]
+    query: Option<String>,
+    /// Read a query from file and print the results of applying the
+    /// format query to each input file.
+    #[structopt(long)]
+    query_file: Option<PathBuf>,
     // The (binary) input files to operate on:
     /// The path to the file(s) to parse
     #[structopt(parse(from_os_str))]
