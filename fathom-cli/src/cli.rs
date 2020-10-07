3a366b9e9dde3a5434a1867fa0bff3b327e04678
Paul <423357+toothbrush@users.noreply.github.com>
2020-10-07 15:06:00 +0930

    Add first stab at introducing the format options we want.
    See #239.

--- fathom-cli/src/cli.rs
+++ fathom-cli/src/cli.rs
@@ -5,8 +5,17 @@ use structopt::StructOpt;
 /// Fathom DDL interpreter to provide tools to inspect and query binary files.
 #[derive(StructOpt, Debug)]
 pub struct Cli {
-    /// The pattern to look for
-    pattern: String,
+    /// --format=NAME is an optional argument for specifying the name
+    /// of the format description in the installed catalog, for
+    /// example "opentype", that will be used to process the files.
+    #[structopt(long)]
+    format: String,
+    /// --format-file=FILE is an optional argument for specifying the
+    /// file containing the format description, for example
+    /// "path/to/myformat.ddl", that will be used to process the
+    /// files.
+    #[structopt(long)]
+    format_file: PathBuf,
     /// The path to the file(s) to parse
     #[structopt(parse(from_os_str))]
     files: Vec<PathBuf>,
