f1af373d95d1091048ac6274bd443a46f6b9a707
Paul <423357+toothbrush@users.noreply.github.com>
2020-10-07 15:31:23 +0930

    Slightly reword help text for format selections.

--- fathom-cli/src/cli.rs
+++ fathom-cli/src/cli.rs
@@ -7,15 +7,15 @@ use structopt::{clap::ArgGroup, StructOpt};
 #[derive(StructOpt, Debug)]
 #[structopt(group = ArgGroup::with_name("format_choice"))]
 pub struct Cli {
-    /// --format=NAME is an optional argument for specifying the name
-    /// of the format description in the installed catalog, for
-    /// example "opentype", that will be used to process the files.
+    // Format choice, optional:
+    /// Optional argument for specifying the name of the format
+    /// description in the installed catalog, for example "opentype",
+    /// that will be used to process the files.
     #[structopt(long, group = "format_choice")]
     format: Option<String>,
-    /// --format-file=FILE is an optional argument for specifying the
-    /// file containing the format description, for example
-    /// "path/to/myformat.ddl", that will be used to process the
-    /// files.
+    /// Optional argument for specifying the file containing the
+    /// format description, for example "path/to/myformat.ddl", that
+    /// will be used to process the files.
     #[structopt(long, group = "format_choice")]
     format_file: Option<PathBuf>,
     // The (binary) input files to operate on:
