b7c32390d8f64b9d7d2b02b0311acfa46f5f9a9b
Paul <423357+toothbrush@users.noreply.github.com>
2020-12-02 15:49:38 +0930

    fathom-cli: Can load file from CLI.

--- fathom-cli/src/main.rs
+++ fathom-cli/src/main.rs
@@ -1,3 +1,4 @@
+use fathom::driver;
 use structopt::StructOpt;
 
 mod cli;
@@ -7,4 +8,19 @@ fn main() {
     println!("Hello, world!");
 
     println!("debug: {:?}", args);
+
+    if args.files.len() == 0 {
+        // error, please provide an input file
+    }
+
+    if args.files.len() != 1 {
+        // note that we only will read one input file.
+    }
+
+    let file = args.files.first().unwrap();
+
+    driver::compiler::run_compiler(
+        &*file.clone().into_os_string().into_string().unwrap(),
+        &*file,
+    )
 }
