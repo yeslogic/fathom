167d4b315497246a4ad4e459158df3c49b3791dd
Paul <423357+toothbrush@users.noreply.github.com>
2020-12-02 15:15:04 +0930

    cli: make struct fields public.

--- fathom-cli/src/cli.rs
+++ fathom-cli/src/cli.rs
@@ -12,35 +12,35 @@ pub struct Cli {
     /// description in the installed catalog, for example "opentype",
     /// that will be used to process the files.
     #[structopt(long, group = "format_choice", name = "NAME")]
-    format: Option<String>,
+    pub format: Option<String>,
     /// Optional argument for specifying the file containing the
     /// format description, for example "path/to/myformat.ddl", that
     /// will be used to process the files.
     #[structopt(long, group = "format_choice", name = "FORMAT-FILE")]
-    format_file: Option<PathBuf>,
+    pub format_file: Option<PathBuf>,
     // Output options, optional:
     /// Prints a brief summary of each file based on the format
     /// description. This may include printing the type or version or
     /// other important details about the file but may not validate
     /// the entire file contents.
     #[structopt(long)]
-    summary: bool,
+    pub summary: bool,
     /// Prints a textual representation of the complete contents of
     /// each file as described by the format description. This will
     /// also validate the entire file against the format description
     /// and warn of any discrepancies.
     #[structopt(long)]
-    dump: bool,
+    pub dump: bool,
     /// Print the results of applying the specified format query to
     /// each input file.
     #[structopt(long, name = "QUERY")]
-    query: Option<String>,
+    pub query: Option<String>,
     /// Read a query from file and print the results of applying the
     /// format query to each input file.
     #[structopt(long, name = "QUERY-FILE")]
-    query_file: Option<PathBuf>,
+    pub query_file: Option<PathBuf>,
     // The (binary) input files to operate on:
     /// The path to the file(s) to parse
     #[structopt(parse(from_os_str))]
-    files: Vec<PathBuf>,
+    pub files: Vec<PathBuf>,
 }
