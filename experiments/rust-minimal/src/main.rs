f4b7dcad82640cc2f9a19ea61b28cb57eb71ca90
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2021-06-10 14:01:55 +1000

    Use pretty printing in CLI

--- experiments/rust-minimal/src/main.rs
+++ experiments/rust-minimal/src/main.rs
@@ -1,4 +1,4 @@
-use fathom_minimal::{elaboration, surface, StringInterner};
+use fathom_minimal::{distillation, elaboration, surface, StringInterner};
 use structopt::StructOpt;
 use typed_arena::Arena;
 
@@ -20,11 +20,27 @@ fn main() {
             let mut context = elaboration::Context::new(&core_arena);
             let surface_term = surface::Term::parse(&mut interner, &surface_arena, &term).unwrap();
 
-            if let Some((core_term, r#type)) = context.synth(&surface_term) {
-                if let Some(core_term) = context.normalize(&core_arena, &core_term) {
-                    // TODO: Pretty print term and type
-                    dbg!(core_term);
-                    dbg!(r#type);
+            if let Some((term, r#type)) = context.synth(&surface_term) {
+                if let (Some(term), Some(r#type)) = (
+                    context.normalize(&core_arena, &term),
+                    context.readback(&core_arena, &r#type),
+                ) {
+                    use pretty::DocAllocator;
+
+                    let mut context = distillation::Context::new(&surface_arena);
+                    let term = context.check(&term);
+                    let r#type = context.synth(&r#type);
+
+                    let doc_arena = pretty::Arena::<()>::new();
+                    let doc = (doc_arena.nil())
+                        .append(term.pretty(&interner, &doc_arena))
+                        .append(doc_arena.space())
+                        .append(doc_arena.text(":"))
+                        .append(doc_arena.space())
+                        .append(r#type.pretty(&interner, &doc_arena))
+                        .into_doc();
+
+                    println!("{}", doc.pretty(usize::MAX));
                 }
             }
 
@@ -39,9 +55,17 @@ fn main() {
 
             let mut context = elaboration::Context::new(&core_arena);
             let surface_term = surface::Term::parse(&mut interner, &surface_arena, &term).unwrap();
+
             if let Some((_, r#type)) = context.synth(&surface_term) {
-                // TODO: Pretty print type
-                dbg!(r#type);
+                if let Some(r#type) = context.readback(&core_arena, &r#type) {
+                    let mut context = distillation::Context::new(&surface_arena);
+                    let r#type = context.synth(&r#type);
+
+                    let doc_arena = pretty::Arena::<()>::new();
+                    let doc = r#type.pretty(&interner, &doc_arena).into_doc();
+
+                    println!("{}", doc.pretty(usize::MAX));
+                }
             }
 
             for message in context.drain_messages() {
