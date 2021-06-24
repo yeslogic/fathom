9a907c1e9e240b50db261c0ff8ad2d723b9e0596
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2021-06-24 21:10:02 +1000

    Obey correct precedences in pretty printer

--- experiments/rust-minimal/src/lib.rs
+++ experiments/rust-minimal/src/lib.rs
@@ -23,7 +23,7 @@
 // - implementation
 //   - [x] command line interface
 //   - [x] parser
-//   - [ ] pretty printing
+//   - [x] pretty printing
 //   - [x] source location tracking
 //   - [x] string interning
 //   - [x] arena allocation
@@ -688,6 +688,16 @@ pub mod surface {
         use crate::surface::Term;
         use crate::{StringId, StringInterner};
 
+        /// Term precedences
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+        pub enum Prec {
+            Top = 0,
+            Let,
+            Fun,
+            App,
+            Atomic,
+        }
+
         pub struct Context<'doc, D> {
             interner: &'doc StringInterner,
             alloc: &'doc D,
@@ -716,76 +726,107 @@ pub mod surface {
 
             pub fn ann(&self, expr: &Term<'_>, r#type: &Term<'_>) -> DocBuilder<'doc, D> {
                 self.concat([
-                    self.concat([self.term(&expr), self.space(), self.text(":")])
-                        .group(),
+                    self.concat([
+                        self.term_prec(Prec::Let, &expr),
+                        self.space(),
+                        self.text(":"),
+                    ])
+                    .group(),
                     self.softline(),
-                    self.term(&r#type),
+                    self.term_prec(Prec::Top, &r#type),
                 ])
             }
 
-            // FIXME: precedences, indentation, and grouping
+            pub fn paren(&self, wrap: bool, doc: DocBuilder<'doc, D>) -> DocBuilder<'doc, D> {
+                if wrap {
+                    self.concat([self.text("("), doc, self.text(")")])
+                } else {
+                    doc
+                }
+            }
+
             pub fn term(&self, term: &Term<'_>) -> DocBuilder<'doc, D> {
+                self.term_prec(Prec::Top, term)
+            }
+
+            pub fn term_prec(&self, prec: Prec, term: &Term<'_>) -> DocBuilder<'doc, D> {
+                // FIXME: indentation and grouping
+
                 match term {
                     Term::Var(_, name) => self.name(*name),
                     Term::Ann(expr, r#type) => self.ann(expr, r#type),
-                    Term::Let(_, (_, def_name), def_type, def_expr, body_expr) => self.concat([
+                    Term::Let(_, (_, def_name), def_type, def_expr, body_expr) => self.paren(
+                        prec > Prec::Let,
                         self.concat([
-                            self.text("let"),
-                            self.space(),
-                            self.name(*def_name),
-                            self.space(),
-                            match def_type {
-                                None => self.nil(),
-                                Some(def_type) => self.concat([
-                                    self.text(":"),
-                                    self.softline(),
-                                    self.term(def_type),
-                                    self.space(),
-                                ]),
-                            },
-                            self.text("="),
-                            self.softline(),
-                            self.term(def_expr),
-                            self.text(";"),
-                        ])
-                        .group(),
-                        self.line(),
-                        self.term(body_expr),
-                    ]),
+                            self.concat([
+                                self.text("let"),
+                                self.space(),
+                                self.name(*def_name),
+                                self.space(),
+                                match def_type {
+                                    None => self.nil(),
+                                    Some(def_type) => self.concat([
+                                        self.text(":"),
+                                        self.softline(),
+                                        self.term_prec(Prec::Fun, def_type),
+                                        self.space(),
+                                    ]),
+                                },
+                                self.text("="),
+                                self.softline(),
+                                self.term_prec(Prec::Let, def_expr),
+                                self.text(";"),
+                            ])
+                            .group(),
+                            self.line(),
+                            self.term_prec(Prec::Let, body_expr),
+                        ]),
+                    ),
                     Term::Universe(_) => self.text("Type"),
-                    Term::FunType(_, (_, input_name), input_type, output_type) => self.concat([
+                    Term::FunType(_, (_, input_name), input_type, output_type) => self.paren(
+                        prec > Prec::Fun,
                         self.concat([
-                            self.text("fun"),
-                            self.space(),
-                            self.text("("),
-                            self.name(*input_name),
-                            self.space(),
-                            self.text(":"),
+                            self.concat([
+                                self.text("fun"),
+                                self.space(),
+                                self.text("("),
+                                self.name(*input_name),
+                                self.space(),
+                                self.text(":"),
+                                self.softline(),
+                                self.term_prec(Prec::Top, input_type),
+                                self.text(")"),
+                                self.space(),
+                                self.text("->"),
+                            ])
+                            .group(),
                             self.softline(),
-                            self.term(input_type),
-                            self.text(")"),
-                            self.space(),
-                            self.text("->"),
-                        ])
-                        .group(),
-                        self.softline(),
-                        self.term(output_type),
-                    ]),
-                    Term::FunIntro(_, (_, input_name), output_expr) => self.concat([
+                            self.term_prec(Prec::Let, output_type),
+                        ]),
+                    ),
+                    Term::FunIntro(_, (_, input_name), output_expr) => self.paren(
+                        prec > Prec::Fun,
                         self.concat([
-                            self.text("fun"),
+                            self.concat([
+                                self.text("fun"),
+                                self.space(),
+                                self.name(*input_name),
+                                self.space(),
+                                self.text("=>"),
+                            ])
+                            .group(),
                             self.space(),
-                            self.name(*input_name),
+                            self.term_prec(Prec::Let, output_expr),
+                        ]),
+                    ),
+                    Term::FunElim(head_expr, input_expr) => self.paren(
+                        prec > Prec::App,
+                        self.concat([
+                            self.term_prec(Prec::App, head_expr),
                             self.space(),
-                            self.text("=>"),
-                        ])
-                        .group(),
-                        self.space(),
-                        self.term(output_expr),
-                    ]),
-                    Term::FunElim(head_expr, input_expr) => {
-                        self.concat([self.term(head_expr), self.space(), self.term(input_expr)])
-                    }
+                            self.term_prec(Prec::Atomic, input_expr),
+                        ]),
+                    ),
                 }
             }
         }
