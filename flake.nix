ac2af119eab614a3658110f06635a84957478ddf
brendanzab <bjzaba@yahoo.com.au>
2021-12-07 13:21:41 +1100

    Use naersk to build nix package from Cargo.lock

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
     # Legacy shim used in `./default.nix` and `./shell.nix`
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
 
+    # Build rust crates from `Cargo.lock` dependencies
+    naersk.url = "github:nmattia/naersk";
+    # Avoid duplicate dependencies in `flake.lock`
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+
     # Rust toolchain
     rust-overlay.url = "github:oxalica/rust-overlay";
     # Avoid duplicate dependencies in `flake.lock`
@@ -16,7 +21,7 @@
     rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay, ... }:
     # Build the output set for each default system and map system sets into
     # attributes, resulting in paths such as:
     #
@@ -26,6 +31,7 @@
         # The rust toolcheckian to use for development
         # TODO: Figure out how to let users select this?
         rustChannel = "1.56.0";
+        crateName = "fathom";
 
         # Package set with the rust overlay included added
         pkgs = import nixpkgs {
@@ -34,6 +40,23 @@
         };
       in
       {
+        # Used by `nix build .#<name>`
+        packages.${crateName} = naersk.lib.${system}.buildPackage {
+          pname = crateName;
+          root = ./.;
+        };
+
+        # Used by `nix run`
+        defaultPackage = self.packages.${system}.${crateName};
+
+        # Used by `nix run .#<name>`
+        apps.${crateName} = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.${crateName};
+        };
+
+        # Executed by `nix run . -- <args?>`
+        defaultApp = self.apps.${system}.${crateName};
+
         # Used by `nix develop`
         devShell = pkgs.mkShell {
           # Tools needed for development go here.
@@ -42,7 +65,7 @@
             pkgs.clippy
           ];
 
-          # Certain Rust tools won't work without this
+          # Certain Rust tools (like `rust-analyzer`) won't work without this
           RUST_SRC_PATH =
             "${pkgs.rust-bin.stable.${rustChannel}.rust-src}/lib/rustlib/src/rust/library";
         };
