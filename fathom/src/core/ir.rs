853a2fd9f503e54eae949d97f139ed829362e727
Wesley Moore <wes@wezm.net>
2022-12-07 14:01:55 +1000

    WIP IR

---
+++ fathom/src/core/ir.rs
@@ -0,0 +1,318 @@
+/// An interned string
+type StringId = u16;
+
+struct Module {
+    items: Vec<Item>,
+}
+
+enum Item {
+    Format(Format),
+}
+
+struct Format {
+    name: StringId,
+    fields: Vec<Field>,
+}
+
+struct Field {
+    name: StringId,
+    host_type: HostType,
+    read: ReadExpr,
+}
+
+#[derive(Clone)]
+enum HostType {
+    /// A built in type
+    Prim(HostPrim),
+    /// A custom type that lives in the context
+    CustomType(usize),
+}
+
+// A host side custom type
+enum CustomType {
+    Record(Record),
+    Enum(Enum),
+}
+
+struct Record {
+    name: StringId,
+    fields: Vec<HostField>,
+}
+
+// a match that can yield different types compiles into an enum
+struct Enum {
+    name: StringId,
+    variants: Vec<Variant>,
+}
+
+struct Variant {
+    name: StringId,
+    data: Option<HostType>,
+}
+
+struct HostField {
+    name: StringId,
+    host_type: HostType, // in theory there could also be write: WriteExpr
+}
+
+#[derive(Clone)]
+enum HostPrim {
+    S16,
+    U16,
+    U32,
+    // An array with length and element type
+    // The length can be another item or a const
+    Array(Val, Box<HostType>), // Perhaps array with const length and array with non-const length should be separated
+}
+
+#[derive(Clone)]
+enum Val {
+    /// Item in the context
+    Item(usize),
+    Const(Const),
+}
+
+#[derive(Clone)]
+enum Const {
+    U8(u8),
+    U16(u16),
+    U32(u32),
+}
+
+struct ReadFn {
+    exprs: Vec<ReadExpr>,
+}
+
+enum ReadExpr {
+    Prim(ReadPrim),
+    /// A custom type that lives in the context
+    CustomType(usize),
+    /// A match expression, cond, branches, default?
+    Match(Val, Vec<Branch>),
+}
+
+struct Branch {
+    pattern: Pattern,
+    expr: Box<ReadExpr>,
+}
+
+enum Pattern {
+    Var(StringId),
+    Const(Const),
+}
+
+enum ReadPrim {
+    S16Be,
+    S16Le,
+    U16Be,
+    U16Le,
+    U32Be,
+    U32Le,
+    Array(Val, Box<ReadExpr>),
+}
+
+impl Format {
+    fn repr(&self) -> CustomType {
+        let fields = self
+            .fields
+            .iter()
+            .map(|field| HostField {
+                name: field.name,
+                host_type: field.host_type.clone(),
+            })
+            .collect();
+        CustomType::Record(Record {
+            name: self.name,
+            fields,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_f2dot14() {
+        // pub struct F2Dot14(u16);
+        // impl<'a> ReadFrom<'a> for F2Dot14 {
+        //     type ReadType = U16Be;
+        //     fn from(value: u16) -> Self {
+        //         F2Dot14(value)
+        //     }
+        // }
+        //
+        // def f2dot14 : Format = s16be;
+        let format = Format {
+            name: 1,
+            fields: vec![Field {
+                name: 1,
+                host_type: HostType::Prim(HostPrim::U16),
+                read: ReadExpr::Prim(ReadPrim::U16Be),
+            }],
+        };
+
+        let item = Item::Format(format);
+        let module = Module { items: vec![item] };
+    }
+
+    #[test]
+    fn test_table_record() {
+        /*
+
+        def table_record = {
+            /// Table identifier.
+            table_id <- tag,
+            /// CheckSum for this table.
+            ///
+            /// ## References
+            ///
+            /// - [Microsoft's OpenType Spec: Calculating Checksums](https://docs.microsoft.com/en-us/typography/opentype/spec/otff#calculating-checksums)
+            checksum <- u32be,
+            /// Offset from the beginning of the TrueType font file.
+            offset <- u32be,
+            /// Length of this table.
+            length <- u32be,
+        };
+
+        pub struct TableRecord {
+            pub table_tag: u32,
+            pub checksum: u32,
+            pub offset: u32,
+            pub length: u32,
+        }
+
+        impl<'a> ReadFrom<'a> for TableRecord {
+            type ReadType = ((U32Be, U32Be), (U32Be, U32Be));
+            fn from(((table_tag, checksum), (offset, length)): ((u32, u32), (u32, u32))) -> Self {
+                TableRecord {
+                    table_tag,
+                    checksum,
+                    offset,
+                    length,
+                }
+            }
+        }
+         */
+
+        // In order to be able to generate a ReadFrom impl we need to know/generate the ReadType as well as the host representation of that
+        // Each field will eventually need a name too. Although in this case the tuple fields can be accessed by index
+        let format = Format {
+            name: 2,
+            fields: vec![
+                Field {
+                    name: 1, // table_tag
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 2, // checksum
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 3, // offset
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 3, // length
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+            ],
+        };
+
+        let item = Item::Format(format);
+        let module = Module { items: vec![item] };
+    }
+
+    #[test]
+    fn offset_table() {
+        /*
+
+        pub struct OffsetTable<'a> {
+            pub sfnt_version: u32,
+            pub search_range: u16,
+            pub entry_selector: u16,
+            pub range_shift: u16,
+            pub table_records: ReadArray<'a, TableRecord>,
+        }
+
+        impl<'a> ReadBinary<'a> for OffsetTable<'a> {
+            type HostType = Self;
+
+            fn read(ctxt: &mut ReadCtxt<'a>) -> Result<Self, ParseError> {
+                let sfnt_version = ctxt.read_u32be()?;
+                match sfnt_version {
+                    TTF_MAGIC | CFF_MAGIC => {
+                        let num_tables = ctxt.read_u16be()?;
+                        let search_range = ctxt.read_u16be()?;
+                        let entry_selector = ctxt.read_u16be()?;
+                        let range_shift = ctxt.read_u16be()?;
+                        let table_records = ctxt.read_array::<TableRecord>(usize::from(num_tables))?;
+                        Ok(OffsetTable {
+                            sfnt_version,
+                            search_range,
+                            entry_selector,
+                            range_shift,
+                            table_records,
+                        })
+                    }
+                    _ => Err(ParseError::BadVersion),
+                }
+            }
+        }
+         */
+
+        // Assume table_record above is in the context at index 0
+
+        let offset_table = Format {
+            name: 3,
+            fields: vec![
+                Field {
+                    name: 1, // num_tables
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 2, // sfnt_version
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 3, // search_range
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 4, // entry_selector
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 5, // range_shift
+                    host_type: HostType::Prim(HostPrim::U32),
+                    read: ReadExpr::Prim(ReadPrim::U32Be),
+                },
+                Field {
+                    name: 6, // table_records
+                    host_type: HostType::Prim(HostPrim::Array(
+                        Val::Item(5 /* num_tables */),
+                        // This one is referring to a custom type in the host env
+                        // Or is it just referring to the format?
+                        Box::new(HostType::CustomType(0)),
+                    )),
+                    read: ReadExpr::Prim(ReadPrim::Array(
+                        Val::Item(5 /* num_tables */),
+                        // This one is referring to a custom type in the "format" env
+                        Box::new(ReadExpr::CustomType(0)),
+                    )),
+                },
+            ],
+        };
+
+        let item = Item::Format(offset_table);
+        let module = Module { items: vec![item] };
+    }
+}
